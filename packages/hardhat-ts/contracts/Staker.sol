d2d73e88a6dff7a1e0d3a190b88f8108790f37bf
TheNuelgeek <thenuelgeekmain@gmail.com>
2022-06-07 02:10:31 +0100

    timeleft added

--- packages/hardhat-ts/contracts/Staker.sol
+++ packages/hardhat-ts/contracts/Staker.sol
@@ -14,59 +14,52 @@ contract Staker {
   // TODO: Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
   bool OpenForWithdrawal;
+
   uint256 public constant threshold = 1 ether;
 
   uint256 public deadline = block.timestamp + 30 seconds;
 
   mapping(address => uint256) public balances;
 
-  uint256 public _timeleft = timeLeft();
-
   event Stake(address indexed _staker, uint256 indexed _amount);
+
+  modifier OpenForWithdrawal_() {
+    bool _OpenForWithdrawal = OpenForWithdrawal;
+    require(_OpenForWithdrawal = true, 'Withdraw not opened yet');
+    _;
+  }
 
   function stake(address staker, uint256 amount) public payable returns (bool success) {
     balances[staker] += amount;
     success = true;
     emit Stake(staker, amount);
-    console.log('balance', balances[staker]);
   }
 
   // TODO: After some `deadline` allow anyone to call an `execute()` function
   //  It should call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
 
   function execute() public {
-    // uint256 _timeleft = timeLeft();
+    require(timeLeft() == 0, 'Not deadline yet');
     if (address(this).balance >= threshold) {
       exampleExternalContract.complete{value: address(this).balance}();
     } else if (address(this).balance < threshold) {
       OpenForWithdrawal = true;
     }
-
-    // if (_timeleft == 0) {
-    // } else {
-    //   // timeLeft();1000000000000000000
-    // }address(this).balance >= threshold)
+    //1000000000000000000
   }
 
-  function withdraw() public {
-    assert(OpenForWithdrawal == true);
+  function withdraw() public OpenForWithdrawal_ {
     uint256 amount = balances[msg.sender];
     (bool os, ) = payable(msg.sender).call{value: amount}('');
     require(os);
   }
 
-  //2994.6710
-  function timeLeft() public view returns (uint256 _deadline) {
-    _deadline = block.timestamp;
-    //deadline = block.timestamp + 30 seconds
-    uint256 newDeadline = deadline - _deadline;
-    for (uint256 i = 30; i > 0; i--) {
-      newDeadline - 1;
-      if (i == 0) {
-        break;
-      }
+  function timeLeft() public view returns (uint256) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    } else {
+      return deadline - block.timestamp;
     }
-    return newDeadline;
   }
 
   // receive()external payable{
